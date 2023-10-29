67b85ec0f08caf2eccba5733caa51ccee1b14f7a
mdrabbi97324 <mdrabbi97324@gmail.com>
2023-10-29 21:35:25 +0600

    added two button in react recap folder

--- react-recap/src/components/countries/Countries.jsx
+++ react-recap/src/components/countries/Countries.jsx
@@ -5,18 +5,48 @@ import './Countries.css'
 const Countries = () => {
 
     const [country, setCountry] = useState([]);
+    const [visitedCountry, setVisitedCountry] = useState([]);
+    const [visitedFlags, setVisitedFlags] = useState([])
     
     useEffect(() => {
         fetch('https://restcountries.com/v3.1/all')
         .then(res => res.json())
         .then(data => setCountry(data))
     }, [])
+
+    const handleVisitedCountry = (country) => {
+        const newVisitedCountry = [...visitedCountry, country];
+        setVisitedCountry(newVisitedCountry);
+    }
+
+    const handleVisitedFlags = (flag) => {
+        const newVisitedFlags = [...visitedFlags, flag];
+        setVisitedFlags(newVisitedFlags);
+    }
+
+
+    
     return (
         <div>
+            {/* Show Flag of visited country */}
+            <div className="flag-container">
+                {
+                    visitedFlags.map(flag => <img key={country.ccn3} src={flag}></img>)
+                }
+
+            </div>
+            <div>
+                <h4>Visited Countries Name : {visitedCountry.length}</h4>
+                <ul>
+                    {
+                        visitedCountry.map(country => <li key={country.cca2}>{country.name.common}</li>)
+                    }
+                </ul>
+            </div>
             <h2>Total Countries : {country.length}</h2>
             <div className={'countries-container'}>
                 {
-                    country.map(country => <Country key={country.cca3} country = {country}></Country>)
+                    country.map(country => <Country key={country.cca3} handleVisitedFlags={handleVisitedFlags} handleVisitedCountry= {handleVisitedCountry} country = {country}></Country>)
                 }
             </div>
         </div>
--- react-recap/src/components/country/Country.jsx
+++ react-recap/src/components/country/Country.jsx
@@ -1,18 +1,23 @@
 import { useState } from 'react';
 import './Country.css'
-export default function Country({country}){
+export default function Country({country, handleVisitedCountry, handleVisitedFlags}){
     console.log(country)
     const [visited, setVisited] = useState(false);
     const handleClick = () => {
         setVisited(!visited)
     }
     const {name, population, flags, cca3} = country;
+    
     return (
-        <div  className={`country ${visited} ? 'visited' : 'non-visit'`}>
+        // <div  className={`country ${visited} ? 'visited' : 'non-visit'`}>
+        <div  className={visited ? 'visited' : 'country'}>
             <img src={flags.png} alt="" />
             <p>Name : {name.common}</p>
             <p>Population : {population}</p>
             <p>Key or cca3 : {cca3}</p>
+            <button onClick={() => handleVisitedFlags(country.flags.png)}>Add Visited Flag</button>
+            <button onClick={()=> handleVisitedCountry(country)}>Add Visited Country</button>
+            <br />
             <button style={{marginRight: '5px'}} onClick={handleClick}>{visited ? 'visited' : 'going'}</button>
             {visited ? 'I have visited this country' : 'I am not visited but going in future'}
         </div>
